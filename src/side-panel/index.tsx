3219cf3bb899dba1d35bc5437ae367cd9a4798cc
SpuerMomonga <1638829841@qq.com>
2024-09-24 17:57:05 +0800

    feat: 增加 theme-provider

--- src/side-panel/index.tsx
+++ src/side-panel/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../globals.css';
 import App from './app';
+import { ThemeProvider } from '@/components/theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
