4d07423aa5971f137fb48c5197b17ae60259c3e1
SpuerMomonga <1638829841@qq.com>
2024-09-24 16:11:32 +0800

    fix: 删除border颜色

--- src/side-panel/layout/sidebar/index.tsx
+++ src/side-panel/layout/sidebar/index.tsx
@@ -3,8 +3,8 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="h-full w-14 flex flex-col p-2 bg-accent rounded-xl">
-      <div>logo</div>
+    <div className="h-full w-14 flex flex-col p-2 border border-l-accent">
+      <div className="border-b-accent">logo</div>
       <div className="flex-auto">icon</div>
       <div>
         <Avatar>
