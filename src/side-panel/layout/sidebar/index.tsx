e0d0511d3661c3105b1311e70e6678c7fce76607
SpuerMomonga <1638829841@qq.com>
2024-09-24 23:26:31 +0800

    feat: 新增 nav

--- src/side-panel/layout/sidebar/index.tsx
+++ src/side-panel/layout/sidebar/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Nav, type LinkItem } from './nav';
+import { Search, Star, Languages } from 'lucide-react';
 
 const Sidebar: React.FC = () => {
+  const links: LinkItem[] = [
+    {
+      key: 'search',
+      title: '搜索',
+      icon: Search,
+    },
+    {
+      key: 'translate',
+      title: '翻译',
+      icon: Languages,
+    },
+    {
+      key: 'collect',
+      title: '收藏',
+      icon: Star,
+    },
+  ];
+
   return (
-    <div className="h-full w-14 flex flex-col p-2 border border-l-accent">
+    <div className="h-full w-14 flex flex-col p-2">
       <div className="border-b-accent">logo</div>
-      <div className="flex-auto">icon</div>
+      <div className="flex-auto">
+        <Nav isCollapsed={false} links={links} />
+      </div>
       <div>
         <Avatar>
           <AvatarImage src="#" alt="SpuerMomonga" />
---
+++ src/side-panel/layout/sidebar/nav.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { LucideIcon } from 'lucide-react';
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+type LinkItem = {
+  key: string;
+  title: string;
+  label?: string;
+  icon: LucideIcon;
+  disabled?: boolean;
+};
+interface NavProps {
+  isCollapsed: boolean;
+  links: LinkItem[];
+  defaultActiveKey?: string;
+  onClick?: (key: string) => void;
+}
+
+const Nav: React.FC<NavProps> = (props) => {
+  const { isCollapsed, links, onClick, defaultActiveKey } = props;
+  const [activeKey, setActiveKey] = useState(defaultActiveKey ?? links[0].key);
+
+  return (
+    <div data-collapsed={isCollapsed} className="flex flex-col py-2">
+      <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center">
+        {links.map((link) =>
+          isCollapsed ? (
+            <Tooltip key={link.key} delayDuration={0}>
+              <TooltipTrigger asChild>
+                <Button
+                  variant={activeKey === link.key ? 'default' : 'ghost'}
+                  onClick={() => {
+                    setActiveKey(link.key);
+                    onClick?.(link.key);
+                  }}
+                >
+                  <link.icon className="h-4 w-4" />
+                  <span className="sr-only">{link.title}</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="right" className="flex items-center gap-4">
+                {link.title}
+                {link.label && <span className="ml-auto text-muted-foreground">{link.label}</span>}
+              </TooltipContent>
+            </Tooltip>
+          ) : (
+            <Button
+              key={link.key}
+              variant={activeKey === link.key ? 'default' : 'ghost'}
+              onClick={() => {
+                setActiveKey(link.key);
+                onClick?.(link.key);
+              }}
+            >
+              <link.icon className="mr-2 h-4 w-4" />
+              {link.title}
+              {link.label && (
+                <span className={cn('ml-auto', activeKey === link.key && 'text-background dark:text-white')}>
+                  {link.label}
+                </span>
+              )}
+            </Button>
+          ),
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export { Nav, type LinkItem };
